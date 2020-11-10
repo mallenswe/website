22f172b7a88c91e5ff58bb9631b082bc238c283e
mallenswe <mallenswe@gmail.com>
2020-11-09 23:17:35 -0800

    dev: MatthewAllenServices .Net Core API Solution

--- MatthewAllenUI/src/app/app.component.ts
+++ MatthewAllenUI/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+@Injectable()
 export class AppComponent {
-  title = 'Matthew Allen';
+  title: string = 'Matthew Allen';
+  baseURL: string = 'https://localhost:44326';
+
+  constructor(
+    httpClient: HttpClient
+  ) {
+    httpClient.get(`${this.baseURL}/WeatherForecast`).subscribe(response => {
+      console.log('WeatheForeast response: ', response);
+    })
+  }
 }
