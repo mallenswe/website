b3cf0c24bed645f9a315c4652ab0e233c4e2567a
mallenswe <mallenswe@gmail.com>
2020-11-10 01:51:47 -0800

    dev: removing pipe import

--- MatthewAllenUI/src/app/services/weather-forecast.service.ts
+++ MatthewAllenUI/src/app/services/weather-forecast.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { pipe, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { WeatherForecast } from '../models/weather-forecast.model';
 
